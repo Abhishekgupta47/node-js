3653d7545aa17e3f23afc7826e7b2d9223a38378
Abhishek Gupta <92316753+Abhishekgupta47@users.noreply.github.com>
2023-03-21 20:50:26 +0530

    fs module complete

--- fsmodule.js
+++ fsmodule.js
@@ -4,7 +4,13 @@ const fs = require('fs');
 //fs.readFileSync('file.txt', 'utf8', (err, data) => {
     //console.log(err, data)
 //})
-const a = fs.readdirSync('file.txt')
+//const a = fs.readdirSync('file.txt')
 
-console.log(a.toString())
+//console.log(a.toString())
+
+// fs.writeFile('file2.txt',"this is a data",()=>{
+//     console.log("written to the file")
+// })
+b=fs.writeFileSync('file2.txt',"This is a data again")
+console.log(b);
 console.log("finished reading file")
