6c5791c33466f623f6506a485ff246f4315266bb
Abhishek Gupta <92316753+Abhishekgupta47@users.noreply.github.com>
2023-03-21 21:55:12 +0530

    ...

--- modulefirst.js
+++ modulefirst.js
@@ -1,2 +1,4 @@
-const simple = require("./modulesecond")
+//const simple = require("./modulesecond.mjs")
+import { simple2 as simple } from "./modulesecond.mjs";
+
 simple();
--- modulesecond.js
+++
@@ -1,4 +0,0 @@
-function simple(){
-    console.log("simple is complex")
-}
-module.exports=simple;
